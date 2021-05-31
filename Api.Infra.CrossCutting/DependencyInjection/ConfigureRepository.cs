c32971dbb9d1d6efa4ceca4eda0189fe8a2b62d2
Edson Shideki Kokado <eskokado@gmail.com>
2021-05-31 07:22:14 -0300

    Api.Infra.CrossCutting - Create DependencyInjection

---
+++ Api.Infra.CrossCutting/DependencyInjection/ConfigureRepository.cs
@@ -0,0 +1,19 @@
+using Api.Domain.Repositories;
+using Api.Infra.Data.Context;
+using Api.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.Infra.CrossCutting.DependencyInjection
+{
+    public class ConfigureRepository
+    {
+        public static void ConfigureDependenciesRepository(IServiceCollection serviceCollection) 
+        {
+            serviceCollection.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+            serviceCollection.AddDbContext<MyContext> (
+                options => options.UseMySql("Server=localhost;Port=3306;Database=dbApiSeries;Uid=root;Pwd=root")
+            );
+        }        
+    }
+}
