cdd97e73f7c9fd2fe6877d09c54f1296839d2235
Edson Shideki Kokado <eskokado@gmail.com>
2021-05-31 08:08:35 -0300

    Api.Infra.Data - Create UserImplementation

---
+++ Api.Infra.Data/Implementations/UserImplementation.cs
@@ -0,0 +1,24 @@
+using System.Threading.Tasks;
+using Api.Domain.Entities;
+using Api.Domain.Repositories;
+using Api.Infra.Data.Context;
+using Api.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Infra.Data.Implementations
+{
+    public class UserImplementation : BaseRepository<UserEntity>, IUserRepository
+    {
+        private DbSet<UserEntity> _dataset;
+
+        public UserImplementation(MyContext context) : base(context)
+        {
+            _dataset = context.Set<UserEntity> ();
+        }
+
+        public async Task<UserEntity> FindByLogin(string email)
+        {
+            return await _dataset.FirstOrDefaultAsync(u => u.Email.Equals(email));
+        }
+    }
+}
