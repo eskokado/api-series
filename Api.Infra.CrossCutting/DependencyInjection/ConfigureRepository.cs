cd454544b895389617903abfe969779f38f0bca9
Edson Shideki Kokado <eskokado@gmail.com>
2021-05-31 08:59:29 -0300

    Api.Infra.CrossCutting - Add LoginService, LoginRepository inject dependency

--- Api.Infra.CrossCutting/DependencyInjection/ConfigureRepository.cs
+++ Api.Infra.CrossCutting/DependencyInjection/ConfigureRepository.cs
@@ -1,5 +1,6 @@
 using Api.Domain.Repositories;
 using Api.Infra.Data.Context;
+using Api.Infra.Data.Implementations;
 using Api.Infra.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,6 +12,8 @@ namespace Api.Infra.CrossCutting.DependencyInjection
         public static void ConfigureDependenciesRepository(IServiceCollection serviceCollection) 
         {
             serviceCollection.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+            serviceCollection.AddScoped<IUserRepository, UserImplementation>();
+            
             serviceCollection.AddDbContext<MyContext> (
                 options => options.UseMySql("Server=localhost;Port=3306;Database=dbApiSeries;Uid=root;Pwd=root")
             );
