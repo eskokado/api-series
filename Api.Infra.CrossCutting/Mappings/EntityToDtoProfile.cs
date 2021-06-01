810fa75c3500f6399145ad9b201839654f505b7f
Edson Shideki Kokado <eskokado@gmail.com>
2021-06-01 07:36:16 -0300

    Api.Infra.CrossCutting - Create AutoMapper Profile

---
+++ Api.Infra.CrossCutting/Mappings/EntityToDtoProfile.cs
@@ -0,0 +1,18 @@
+using Api.Domain.Dtos;
+using Api.Domain.Entities;
+using AutoMapper;
+
+namespace Api.Infra.CrossCutting.Mappings
+{
+    public class EntityToDtoProfile : Profile
+    {
+        public EntityToDtoProfile()
+        {
+            CreateMap<UserDto, UserEntity>().ReverseMap();
+
+            CreateMap<UserDtoCreateResult, UserEntity>().ReverseMap();
+
+            CreateMap<UserDtoUpdateResult, UserEntity>().ReverseMap();
+        }
+    }
+}
