6b5bee0b68b4a1ee6eb76bf85fbd8dfb5a576a95
Edson Shideki Kokado <eskokado@gmail.com>
2021-06-02 06:59:24 -0300

    Api.Application.Test - Implement UserController POST tests (Created)

---
+++ Api.Application.Test/User/WhenToRequestCreated/ReturnBadRequest.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Threading.Tasks;
+using Api.Application.Controllers;
+using Api.Domain.Dtos;
+using Api.Domain.Services.User;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace Api.Application.Test.User.WhenToRequestCreated
+{
+    public class ReturnBadRequest
+    {
+        private UsersController _controller;
+
+        [Fact(DisplayName = "É possivel realizar o Created")]
+        public async Task It_is_possible_to_create_Created()
+        {
+            var serviceMock = new Mock<IUserService>();
+            var name = Faker.Name.FullName();
+            var email = Faker.Internet.Email();
+
+            serviceMock
+                .Setup(m => m.Post(It.IsAny<UserDtoCreate>()))
+                .ReturnsAsync(new UserDtoCreateResult {
+                    Id = Guid.NewGuid(),
+                    Name = name,
+                    Email = email,
+                    CreateAt = DateTime.UtcNow
+                });
+
+            _controller = new UsersController(serviceMock.Object);
+            _controller
+                .ModelState
+                .AddModelError("Name", "É um campo obrigatório");
+
+            Mock<IUrlHelper> url = new Mock<IUrlHelper>();
+            url
+                .Setup(x => x.Link(It.IsAny<string>(), It.IsAny<object>()))
+                .Returns("http://localhost:5000");
+            _controller.Url = url.Object;
+
+            var userDtoCreate =
+                new UserDtoCreate { Name = name, Email = email };
+
+            var result = await _controller.Post(userDtoCreate);
+            Assert.True(result is BadRequestObjectResult);
+        }
+    }
+}
