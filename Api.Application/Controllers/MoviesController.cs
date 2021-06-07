26788344f73468f221fb9af88e835c2a4009c122
Edson Shideki Kokado <eskokado@gmail.com>
2021-06-07 10:27:32 -0300

    Api.Application - Implement MoviesController

---
+++ Api.Application/Controllers/MoviesController.cs
@@ -0,0 +1,215 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Api.Domain.Dtos.Movie;
+using Api.Domain.Services.Movie;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieService _service;
+        public MoviesController(IMovieService service)
+        {
+            _service = service;
+        }
+
+        [Authorize("Bearer")]
+        [HttpGet]
+        public async Task<ActionResult> GetAll() 
+        {
+            if (!ModelState.IsValid) 
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(await _service.GetAll());
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode ((int) HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+
+        [Authorize("Bearer")]
+        [HttpGet]
+        [Route("{id}", Name = "GetMovieWithId")]
+        public async Task<ActionResult> Get(Guid id) 
+        {
+            if (!ModelState.IsValid) 
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var result = await _service.Get(id);
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                return Ok(result);
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode ((int) HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] MovieDtoCreate dto) 
+        {
+            if (!ModelState.IsValid) 
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var result = await _service.Post(dto);
+                if (result != null) 
+                {
+                    return Created(new Uri(Url.Link("GetMovieWithId", new {id = result.Id})), result);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode ((int) HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [Authorize("Bearer")]
+        [HttpPut]
+        public async Task<ActionResult> Put([FromBody] MovieDtoUpdate dto) 
+        {
+            if (!ModelState.IsValid) 
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var result = await _service.Put(dto);
+                if (result == null)
+                    return BadRequest();
+
+                return Ok(result);
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode ((int) HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [Authorize("Bearer")]
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<ActionResult> Delete(Guid id) 
+        {
+            if (!ModelState.IsValid) 
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(await _service.Delete(id));
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode ((int) HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [Authorize("Bearer")]
+        [HttpGet]
+        [Route("findByName/{name}")]
+        public async Task<ActionResult> FindCompleteByName(string name) 
+        {
+            if (!ModelState.IsValid) 
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(await _service.FindCompleteByName(name));
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode ((int) HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [Authorize("Bearer")]
+        [HttpGet]
+        [Route("findByDescription/{description}")]
+        public async Task<ActionResult> FindCompleteByDescription(string description) 
+        {
+            if (!ModelState.IsValid) 
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(await _service.FindCompleteByDescription(description));
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode ((int) HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [Authorize("Bearer")]
+        [HttpGet]
+        [Route("findByNameOrDescription/{text}")]
+        public async Task<ActionResult> FindCompleteByNameOrDescription(string text) 
+        {
+            if (!ModelState.IsValid) 
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(await _service.FindCompleteByNameAndDescription(text));
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode ((int) HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [Authorize("Bearer")]
+        [HttpGet]
+        [Route("findByGenreName/{name}")]
+        public async Task<ActionResult> FindCompleteByGenreName(string name) 
+        {
+            if (!ModelState.IsValid) 
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(await _service.FindCompleteByGenreName(name));
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode ((int) HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+    }
+}
