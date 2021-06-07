117107e71270a25dc63d5e88446ca95875c96dcd
Edson Shideki Kokado <eskokado@gmail.com>
2021-06-07 09:23:10 -0300

    Api.Application - Implement GenresController

---
+++ Api.Application/Controllers/GenresController.cs
@@ -0,0 +1,155 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Api.Domain.Dtos.Genre;
+using Api.Domain.Services.Genre;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenresController : ControllerBase
+    {
+        private readonly IGenreService _service;
+        public GenresController(IGenreService service)
+        {
+            _service = service;
+        }
+
+        [Authorize("Bearer")]
+        [HttpGet]
+        public async Task<ActionResult> GetAll() 
+        {
+            if (!ModelState.IsValid) 
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(await _service.GetAll());
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode ((int) HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+
+        [Authorize("Bearer")]
+        [HttpGet]
+        [Route("{id}", Name = "GetGenreWithId")]
+        public async Task<ActionResult> Get(Guid id) 
+        {
+            if (!ModelState.IsValid) 
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var result = await _service.Get(id);
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                return Ok(result);
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode ((int) HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] GenreDtoCreate dto) 
+        {
+            if (!ModelState.IsValid) 
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var result = await _service.Post(dto);
+                if (result != null) 
+                {
+                    return Created(new Uri(Url.Link("GetGenreWithId", new {id = result.Id})), result);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode ((int) HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [Authorize("Bearer")]
+        [HttpPut]
+        public async Task<ActionResult> Put([FromBody] GenreDtoUpdate dto) 
+        {
+            if (!ModelState.IsValid) 
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var result = await _service.Put(dto);
+                if (result == null)
+                    return BadRequest();
+
+                return Ok(result);
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode ((int) HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [Authorize("Bearer")]
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<ActionResult> Delete(Guid id) 
+        {
+            if (!ModelState.IsValid) 
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(await _service.Delete(id));
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode ((int) HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [Authorize("Bearer")]
+        [HttpGet]
+        [Route("find/{name}")]
+        public async Task<ActionResult> FindByName(string name) 
+        {
+            if (!ModelState.IsValid) 
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(await _service.FindByName(name));
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode ((int) HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+    }
+}
--- Api.Infra.CrossCutting/DependencyInjection/ConfigureRepository.cs
+++ Api.Infra.CrossCutting/DependencyInjection/ConfigureRepository.cs
@@ -17,12 +17,18 @@ namespace Api.Infra.CrossCutting.DependencyInjection
             serviceCollection.AddScoped<IMovieRepository, MovieImplementation>();
             serviceCollection.AddScoped<IUserMoviesRepository, UserMoviesImplementation>();
             
+            var connectionString = 
+                "Server=localhost;Port=3306;Database=dbApiSeries;Uid=root;Pwd=root";
+
+            // var connectionString =
+            //     "Server=(localdb)\\mssqllocaldb;Database=dbApiSeries;Trusted_Connection=True;MultipleActiveResultSets=true;user=sa;password=sa@123456";
+
             serviceCollection.AddDbContext<MyContext> (
                 options => 
                 
-                // options.UseMySql("Server=localhost;Port=3306;Database=dbApiSeries;Uid=root;Pwd=root")
-                options
-                    .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=dbApiSeries;Trusted_Connection=True;MultipleActiveResultSets=true;user=sa;password=sa@123456")
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+                // options
+                //     .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=dbApiSeries;Trusted_Connection=True;MultipleActiveResultSets=true;user=sa;password=sa@123456")
             );
         }        
     }
